86b9d317a40f071c333a94e1287f7e44eed23528
frazier.xiao <frazier.xiao@tyj365.com>
2018-12-27 16:10:52 +0800

    update

--- start.sh
+++ start.sh
@@ -7,11 +7,13 @@ result=$(sed -n '2p' ./panyubus.log)
 # head -1 ./panyubus.log
 # 如果不相等，即 Cron 尚未初始化
 if [[ $result != $response ]]; then
+    echo "Cron 尚未初始化，正在尝试初始化....."
     chmod +x cron.sh
     ./cron.sh
 fi
 
 # 打印日志
+echo " " >> panyubus.log
 date >> panyubus.log
 echo "----------------------------------------------------" >> panyubus.log
 python panyubus.py >> panyubus.log 2>&1
