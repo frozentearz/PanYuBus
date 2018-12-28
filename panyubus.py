4170374a412bc02c44eee53e2f7740f1c7cb62b7
frazier.xiao <frazier.xiao@tyj365.com>
2018-12-28 15:30:02 +0800

    修改if

--- panyubus.py
+++ panyubus.py
@@ -26,7 +26,7 @@ def main():
                 if station.get("n") == my_station:
                     flag = stations.index(station)
             bus_status = stations[flag-before_num].get("bus_comming")
-            if len(bus_status.get("bl")) == 0 && len(bus_status.get("bbl") == 0):
+            if len(bus_status.get("bl")) == 0 & len(bus_status.get("bbl") == 0):
                 pass
             else: 
                 Results["msg"] = "已到达" + stations[flag-before_num].get("n") + "，距您仅有 " + before_num + " 站，请做好上车准备！"
