a1bfd7adfc21f3e01c36adf9cde97cdac2a1cdf1
frazier.xiao <frazier.xiao@tyj365.com>
2018-12-28 10:43:08 +0800

    update

--- panyubus.py
+++ panyubus.py
@@ -9,5 +9,5 @@ reload(sys)
 sys.setdefaultencoding('UTF-8')
 
 pan68_url = u"http://h5.thecampus.cc/api/v1/line?line_number=3150&direction=1"
-dict=str(ast.literal_eval(requests.get(pan68_url).content.decode('utf-8')),'utf-8')
+dict=ast.literal_eval(requests.get(pan68_url).content.encode().decode('utf-8'))
 print(dict)
