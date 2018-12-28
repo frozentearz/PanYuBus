8339963371c39a3faca7ce39cdc04ed02fbf8f1d
frazier.xiao <frazier.xiao@tyj365.com>
2018-12-28 11:14:47 +0800

    update

--- panyubus.py
+++ panyubus.py
@@ -5,5 +5,5 @@ import json
 import ast
 
 pan68_url = u"http://h5.thecampus.cc/api/v1/line?line_number=3150&direction=1"
-dict=ast.literal_eval(requests.get(pan68_url).content)
+dict=ast.literal_eval(requests.get(pan68_url).content.decode('utf-8'))
 print(dict.decode('utf-8'))
