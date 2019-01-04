4d74c7f67326be5e89be16dc1083c8a494d03ff7
frazier.xiao <frazier.xiao@tyj365.com>
2019-01-04 16:48:10 +0800

    完成，不想写了

--- panyubus.py
+++ panyubus.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import ConfigParser
+import configparser
 import requests
+import random
 import itchat
 import json
 import time
 import ast
+import os
+
+from requests_toolbelt import MultipartEncoder
 
 def get_img():
     image = {}
@@ -23,16 +27,17 @@ def get_img():
     )
     headers['Content-Type'] = multipart_encoder.content_type
     response = requests.post(url, data=multipart_encoder, headers=headers)
-    
+    json_response = response.content.decode()
+    dict_json = json.loads(json_response)
     if response.status_code != 200:
         print("图片上传失败")
     else:
-        if response.text.get("code") != "success" :
+        if dict_json.get("code") != "success" :
             print("图片上传失败")
         else:
-            image["img_url"] = response.text.get("data").get("url")
-            image["del_url"] = response.text.get("data").get("delete")
-    return image
+            image["img_url"] = dict_json.get("data").get("url")
+            image["del_url"] = dict_json.get("data").get("delete")
+    return {"img_url" : image.get("img_url"), "del_url": image.get("del_url")}
 
 def getDirection(bus):
     l = bus.get("l")
@@ -40,7 +45,7 @@ def getDirection(bus):
     endStation = l[len(l)-1].get("n")
     return startStation + "至" + endStation
 
-def main():
+def send_msg_to_user():
     # 这些是以后扩展时外部传进数据
     url = u"http://h5.thecampus.cc/api/v1/line?line_number=3150&direction=0"
     my_station = "石岗东村站"
@@ -65,42 +70,62 @@ def main():
                 flag = stations.index(station)
         bus_status = stations[flag-before_num].get("bus_comming")
         if len(bus_status.get("bl")) == 0 and len(bus_status.get("bbl")) == 0:
+            Results["status"] = 0
             Results["msg"] = "还未达到，请耐心等候..."
         else: 
-            Results["msg"] = "已到达" + stations[flag-before_num].get("n") + "，距您仅有 " + before_num + " 站，请做好上车准备！"
+            Results["status"] = 1
+            Results["msg"] = "已到达" + stations[flag-before_num].get("n") + "，距您仅有 " + str(before_num) + " 站，请做好上车准备！"
     return Results
 
-def send_QR_to_email():
-    cf = ConfigParser.ConfigParser()
+def qrCallback(uuid, status, qrcode):
+    print("status: "+status)
+    if status == '0':
+        global qrSource
+        qrSource = qrcode
+        itchat.get_QR(uuid)
+        send_QR()
+    elif status == '200':
+        qrSource = 'Logged in!'
+    elif status == '201':
+        qrSource = 'Confirm'
+
+def send_QR():
+    print("sending qrcode")
+    cf = configparser.ConfigParser()
     if os.path.exists("./config/myconfig.conf"):
         cf.read("./config/myconfig.conf")
     else:
         cf.read("./config/config.conf")
     opts = cf.options("mailgun")
     api = cf.get("mailgun", "api")
-    print("api已取出：" + api)
-
     image = get_img()
-    print(image)
     html = "<h2>扫一扫登录微信</h2><img src='" + image.get("img_url") + "'>"
     requests.post(
         "https://api.mailgun.net/v3/mail.dgcontinent.com/messages",
         auth=("api", api),
-        data={"from": "Excited User <mailgun@mail.dgcontinent.com>",
+        data={"from": "Wechat 微信登录授权申请 <mailgun@test.com>",
               "to": ["frozen_tearz@163.com"],
               "subject": "Wechat 微信登录授权申请",
               "html": html
         }
     )
 
+def main():
+    itchat.auto_login(hotReload=True, qrCallback=qrCallback)
+    while 1:
+        Results = send_msg_to_user()
+        if int(Results.get("status")):
+            userMsg = "您好!\n您想搭乘" + Results.get("direction") + "的" + Results.get("line") + "公交车" + Results.get("msg")
+            users=itchat.search_friends("Frazier")
+            userName= users[0]['UserName']
+            itchat.send(userMsg, toUserName=userName)
+            time.sleep(60)
+        else:
+            pass
+            time.sleep(5)
+
 
 if __name__ == '__main__':
-    itchat.auto_login(hotReload=True)
-    while 1:
-        Results = main()
-        userMsg = "您好!\n您想搭乘" + Results.get("direction") + "的" + Results.get("line") + "公交车" + Results.get("msg")
-        users=itchat.search_friends("林豪")
-        userName= users[0]['UserName']
-        itchat.send(userMsg, toUserName=userName)
-        time.sleep(10)
+    main()
+    
     
